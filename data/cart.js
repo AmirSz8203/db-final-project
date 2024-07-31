733fc4e60ffd55415ed59b9f8908d48959105554
amirsz8203 <amir.82.safar@gmail.com>
2024-07-31 22:39:40 +0330

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export const cart = [
+  {
+    productId: "id1",
+    quantity: 2,
+  },
+  {
+    productId: "id2",
+    quantity: 1,
+  },
+];
 
 export function addToCart(productId, quantity) {
   let matchingItem;
